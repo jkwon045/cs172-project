73f6be96c0e17ce4209cdfbe1d8aa1675e0874e6
Jasmine Kwong <jckwong0703@gmail.com>
2019-04-30 15:23:03 -0700

    Rebased using scrapy library

--- cs172_web_scrapper/cs172_web_scrapper/spiders/web-crawler.py
+++ cs172_web_scrapper/cs172_web_scrapper/spiders/web-crawler.py
@@ -2,9 +2,10 @@ from bs4 import BeautifulSoup
 import threading
 import requests
 import sys
+import time
 
 def get_restrictions(url):
-	robots_txt = url[:len(url)] + "/robots.txt"
+	robots_txt = url[:len(url)] + "robots.txt"
 	robots_page = requests.get(robots_txt)
 	parsed_robots_txt = BeautifulSoup(robots_page.content, 'html.parser')
 	string_parsed_robots_txt = parsed_robots_txt.string
@@ -27,23 +28,39 @@ def main():
 	#seed = sys.argv[1]
 	#num_pages = sys.argv[2]
 	#hops = sys.arv[3]
-	seed = "https://www.fda.gov/drugs/information-drug-class/opioid-medications"
-	list_restrictions = get_restrictions("https://www.fda.gov")
+	seed = "https://www.fda.gov/"
+	link = seed[:len(seed)-1]
+	list_restrictions = get_restrictions("https://www.fda.gov/")
 	list_links = []
+	level_dictionary = { seed:1 }
+	level = 0
+	
+	cnt = 0
 
 	list_links.append(seed)
-	cnt = 0
 	for i in list_links:
-		print(i)
-		#print(i)
-		if ( cnt == 1000 ):
-			break;
+		print("Current Page: ", i)
+		if ( level_dictionary[i] > 5 ):
+			break
 		current_page = requests.get(i)
+		if(current_page.status_code != 200 ):
+			continue
 		html_code = BeautifulSoup(current_page.content, 'html.parser')
 		#for tag in html_code.findAll('p'):
 		#	print(tag.get_text())
 		for tag in html_code.findAll('a', href=True):
-			if ( tag['href'] not in list_restrictions and tag['href'][0] == '/'):
-				list_links.append(i+tag['href'])
-		cnt+=1
+			if ( link + tag['href'] not in list_links ):
+				if ( len(tag['href']) > 0 and tag['href'] not in list_restrictions and tag['href'][0] == '/'):
+					if ( len(tag['href']) > 1 ):
+						if ( tag['href'][1] != '['):
+							list_links.append(link+tag['href'])
+							level_dictionary[link+tag['href']] = level_dictionary[i]+1
+						else:
+							continue
+					else:
+						list_links.append(link+tag['href'])
+						level_dictionary[link+tag['href']] = level_dictionary[i]+1
+		cnt += 1
+		time.sleep(30)
+		
 main()
